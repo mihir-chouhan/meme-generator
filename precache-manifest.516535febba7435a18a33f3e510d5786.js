7282011d7ddb49b369ee1e2c519d6e58e41891f2
Mihir <mihir.chouhan@somaiya.edu>
2019-06-20 12:04:50 +0530

    Updates

---
+++ precache-manifest.516535febba7435a18a33f3e510d5786.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bfb9c59bef63a96774144c4a5ff933f",
+    "url": "/meme-generator/index.html"
+  },
+  {
+    "revision": "daa6b8d922611c7eb224",
+    "url": "/meme-generator/static/css/main.49848bc0.chunk.css"
+  },
+  {
+    "revision": "312a939e1847fa5c55aa",
+    "url": "/meme-generator/static/js/2.7b54be8e.chunk.js"
+  },
+  {
+    "revision": "daa6b8d922611c7eb224",
+    "url": "/meme-generator/static/js/main.85105815.chunk.js"
+  },
+  {
+    "revision": "b8372d4f76d85c66a91a",
+    "url": "/meme-generator/static/js/runtime~main.0d1394e5.js"
+  }
+]);
